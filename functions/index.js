92fff38d14cb61d20aba5aebce2568d8e76107d9
yusufkalayci <yb.kalayci@gmail.com>
2020-12-14 14:17:48 -0500

    firebase config added

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,6 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const nodemailer = require('nodemailer');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -7,3 +9,56 @@ const functions = require('firebase-functions');
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+//to make it work you need gmail account
+const gmailEmail = functions.config().gmail.login;
+const gmailPassword = functions.config().gmail.pass;
+
+admin.initializeApp();
+
+//creating function for sending emails
+const goMail = (message) => {
+  //transporter is a way to send your emails
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: gmailEmail,
+      pass: gmailPassword,
+    },
+  });
+
+  // setup email data with unicode symbols
+  //this is how your email are going to look like
+  const mailOptions = {
+    from: gmailEmail, // sender address
+    to: 'joseph.kalayci@gmail.com', // list of receivers
+    subject: 'You have message from your personalpage', // Subject line
+    text: '!' + message, // plain text body
+    html: '!' + message, // html body
+  };
+
+  //this is callback function to return status to firebase console
+  const getDeliveryStatus = (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log('Message sent: %s', info.messageId);
+    return null;
+    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+  };
+
+  //call of this function send an email, and return status
+  transporter.sendMail(mailOptions, getDeliveryStatus);
+};
+
+//.onDataAdded is watches for changes in database
+exports.onDataAdded = functions.firestore
+  .document('/messages/{messageId}')
+  .onCreate((snap, context) => {
+    //here we catch a new data, added to firebase database, it stored in a snap variable
+    const createdData = snap.data();
+    var text = createdData.mail;
+
+    //here we send new data using function for sending emails
+    goMail(text);
+  });
--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Button, Container } from '@material-ui/core';
 import clsx from 'clsx';
+import db from '../firebase/firebase';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -47,12 +48,51 @@ const useStyles = makeStyles((theme) => ({
 const ContactForm = () => {
   const classes = useStyles();
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const message = event.target.message.value;
+    console.log(name, email, message);
+
+    // Add a new document with a generated id.
+    db.collection('messages')
+      .add({
+        name: name,
+        email: email,
+        message: message,
+      })
+      .then(function (docRef) {
+        event.target.reset();
+      })
+      .catch(function (error) {
+        alert(`Opps! something went wrong. Please try agail later\n ${error}`);
+        console.error('Error adding document: ', error);
+      });
+  };
+
   return (
-    <Container component='form' maxWidth='sm' className={classes.root}>
-      <input placeholder='Name' type='text' className={classes.input} />
-      <input placeholder='Enter email' type='email' className={classes.input} />
+    <Container
+      component='form'
+      maxWidth='sm'
+      className={classes.root}
+      onSubmit={handleSubmit}
+    >
+      <input
+        placeholder='Name'
+        name='name'
+        type='text'
+        className={classes.input}
+      />
+      <input
+        placeholder='Enter email'
+        name='email'
+        type='email'
+        className={classes.input}
+      />
       <textarea
         placeholder='Your Message'
+        name='message'
         type='text'
         className={clsx(classes.input, classes.textArea)}
       />
